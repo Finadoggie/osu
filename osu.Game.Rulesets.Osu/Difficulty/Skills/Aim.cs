68d1b53a40b7e83e61cad851841086741ba2d6fe
Kris <moyazuk@gmail.com>
2025-05-26 23:28:27 -0400

    aim rebal

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -17,47 +17,90 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// </summary>
     public class Aim : OsuStrainSkill
     {
-        public readonly bool IncludeSliders;
-
-        public Aim(Mod[] mods, bool includeSliders)
+        public Aim(Mod[] mods, bool withSliders)
             : base(mods)
         {
-            IncludeSliders = includeSliders;
+            this.IncludeSliders = withSliders;
         }
 
-        private double currentStrain;
-
-        private double skillMultiplier => 25.6;
-        private double strainDecayBase => 0.15;
+        public readonly bool IncludeSliders;
 
         private readonly List<double> sliderStrains = new List<double>();
 
-        private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
+        private readonly List<double> previousStrains = new List<double>();
 
+        private double currentStrain;
+        private double currentAngleStrain;
+
+        private double strainDecayBase => 0.15;
+        private double strainDecayBaseAngle => 0.45;
+        private double strainIncreaseRate => 10;
+        private double strainDecreaseRate => 3;
+        private double strainInfluence => 6.0 / 1;
+
+        private double angleStrainInfluence => 8.0 / 1;
+
+        private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
+        private double strainDecayAngle(double ms) => Math.Pow(strainDecayBaseAngle, ms / 1000);
         protected override double CalculateInitialStrain(double time, DifficultyHitObject current) => currentStrain * strainDecay(time - current.Previous(0).StartTime);
 
         protected override double StrainValueAt(DifficultyHitObject current)
         {
-            currentStrain *= strainDecay(current.DeltaTime);
-            currentStrain += AimEvaluator.EvaluateDifficultyOf(current, IncludeSliders) * skillMultiplier;
+            double currentDifficulty = AimEvaluator.EvaluateDistanceDifficultyOf(current, IncludeSliders) * 17.025;
+            currentAngleStrain *= strainDecayAngle(current.DeltaTime);
+            currentAngleStrain += AimEvaluator.EvaluateAngleDifficultyOf(current, IncludeSliders);
+
+            double priorDifficulty = highestPreviousStrain(current, current.DeltaTime);
+
+            currentStrain = getStrainValueOf(currentDifficulty, priorDifficulty);
+            previousStrains.Add(currentStrain);
 
             if (current.BaseObject is Slider)
+            {
                 sliderStrains.Add(currentStrain);
+            }
 
-            return currentStrain;
+            return currentDifficulty + currentStrain * strainInfluence + currentAngleStrain * angleStrainInfluence;
         }
 
+        private double getStrainValueOf(double currentDifficulty, double priorDifficulty) => currentDifficulty > priorDifficulty
+            ? (priorDifficulty * strainIncreaseRate + currentDifficulty) / (strainIncreaseRate + 1)
+            : (priorDifficulty * strainDecreaseRate + currentDifficulty) / (strainDecreaseRate + 1);
+
+        private double highestPreviousStrain(DifficultyHitObject current, double time)
+        {
+            double hardestPreviousDifficulty = 0;
+            double cumulativeDeltaTime = time;
+
+            double timeDecay(double ms) => Math.Pow(strainDecayBase, Math.Pow(ms / 900, 7));
+
+            for (int i = 0; i < previousStrains.Count; i++)
+            {
+                if (cumulativeDeltaTime > 1200)
+                {
+                    previousStrains.RemoveRange(0, i);
+                    break;
+                }
+
+                hardestPreviousDifficulty = Math.Max(hardestPreviousDifficulty, previousStrains[^(i + 1)] * timeDecay(cumulativeDeltaTime));
+
+                cumulativeDeltaTime += current.Previous(i).DeltaTime;
+            }
+
+            return hardestPreviousDifficulty;
+        }
         public double GetDifficultSliders()
         {
             if (sliderStrains.Count == 0)
                 return 0;
 
-            double maxSliderStrain = sliderStrains.Max();
+            double[] sortedStrains = sliderStrains.OrderDescending().ToArray();
 
+            double maxSliderStrain = sortedStrains.Max();
             if (maxSliderStrain == 0)
                 return 0;
 
-            return sliderStrains.Sum(strain => 1.0 / (1.0 + Math.Exp(-(strain / maxSliderStrain * 12.0 - 6.0))));
+            return sortedStrains.Sum(strain => 1.0 / (1.0 + Math.Exp(-(strain / maxSliderStrain * 12.0 - 6.0))));
         }
 
         public double CountTopWeightedSliders() => OsuStrainUtils.CountTopWeightedSliders(sliderStrains, DifficultyValue());
