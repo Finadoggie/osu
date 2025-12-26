edb7c7c492596e5cf473a3963368737239a2051d
Nathan Corbett <75299710+Finadoggie@users.noreply.github.com>
2025-12-25 18:32:53 -0800

    Stop killing bursts

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -19,13 +19,20 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// </summary>
     public class Speed : Skill
     {
-        private double skillMultiplier => 0.93;
+        private double skillMultiplier => 0.8727;
 
         private readonly List<double> noteDifficulties = new List<double>();
 
         private readonly List<double> sliderStrains = new List<double>();
 
+        private double currentStrain;
         private double currentDifficulty;
+        private double currentRhythm;
+
+        private double numNotes;
+        private double numSliders;
+
+        private double currDeltaTime;
 
         private double noteWeightSum;
 
@@ -40,17 +47,58 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         public override void Process(DifficultyHitObject current)
         {
-            currentDifficulty *= strainDecay(((OsuDifficultyHitObject)current).AdjustedDeltaTime);
-            currentDifficulty += SpeedEvaluator.EvaluateDifficultyOf(current, Mods) * skillMultiplier;
+            double prevStrain = currentStrain;
+            double prevDifficulty = currentDifficulty;
+            double prevRhythm = currentRhythm;
 
-            double currentRhythm = RhythmEvaluator.EvaluateDifficultyOf(current);
+            double prevDeltaTime = currDeltaTime;
+            currDeltaTime = ((OsuDifficultyHitObject)current).AdjustedDeltaTime;
 
-            double totalDifficulty = currentDifficulty * currentRhythm;
+            currentDifficulty = SpeedEvaluator.EvaluateDifficultyOf(current, Mods) * skillMultiplier;
 
-            if (current.BaseObject is Slider)
-                sliderStrains.Add(totalDifficulty);
+            currentStrain *= strainDecay(currDeltaTime);
+            currentStrain += currentDifficulty;
 
-            noteDifficulties.Add(totalDifficulty);
+            currentRhythm = RhythmEvaluator.EvaluateDifficultyOf(current);
+
+            double currTotalDifficulty = currentStrain * currentRhythm;
+            double prevTotalDifficulty = prevStrain * prevRhythm;
+
+            if (currTotalDifficulty < prevTotalDifficulty || !(Math.Max(currDeltaTime, prevDeltaTime) < 1.25 * Math.Min(currDeltaTime, prevDeltaTime) && Math.Max(currentDifficulty, prevDifficulty) < 1.1 * Math.Min(currentDifficulty, prevDifficulty)))
+            {
+                for (int i = 0; i < numNotes; i++)
+                    noteDifficulties.Add(prevTotalDifficulty);
+                for (int i = 0; i < numSliders; i++)
+                    sliderStrains.Add(prevTotalDifficulty);
+
+                numNotes = 1;
+                numSliders = 1;
+            }
+            else
+            {
+                numNotes++;
+                if (current.BaseObject is Slider) numSliders++;
+            }
+        }
+
+        protected IEnumerable<double> GetCurrentNoteDifficulties()
+        {
+            IEnumerable<double> strains = noteDifficulties;
+
+            for (int i = 0; i < numNotes; i++)
+                strains = strains.Append(currentStrain * currentRhythm);
+
+            return strains;
+        }
+
+        protected IEnumerable<double> GetCurrentSliderDifficulties()
+        {
+            IEnumerable<double> strains = noteDifficulties;
+
+            for (int i = 0; i < numSliders; i++)
+                strains = strains.Append(currentStrain * currentRhythm);
+
+            return strains;
         }
 
         public override double DifficultyValue()
@@ -59,7 +107,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
             // Notes with 0 difficulty are excluded to avoid worst-case time complexity of the following sort (e.g. /b/2351871).
             // These notes will not contribute to the difficulty.
-            var peaks = noteDifficulties.Where(p => p > 0);
+            var peaks = GetCurrentNoteDifficulties().Where(p => p > 0);
 
             List<double> notes = peaks.ToList();
 
@@ -85,7 +133,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         /// </summary>
         public double CountTopWeightedNotes(double difficultyValue)
         {
-            if (noteDifficulties.Count == 0)
+            List<double> notes = GetCurrentNoteDifficulties().Where(p => p > 0).ToList();
+
+            if (notes.Count == 0)
                 return 0.0;
 
             if (noteWeightSum == 0)
@@ -97,24 +147,28 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
                 return 0;
 
             // Use a weighted sum of all notes. Constants are arbitrary and give nice values
-            return noteDifficulties.Sum(s => 1.1 / (1 + Math.Exp(-10 * (s / consistentTopNote - 0.88))));
+            return notes.Sum(s => 1.1 / (1 + Math.Exp(-10 * (s / consistentTopNote - 0.88))));
         }
 
         public double RelevantNoteCount()
         {
-            if (noteDifficulties.Count == 0)
+            List<double> notes = GetCurrentNoteDifficulties().Where(p => p > 0).ToList();
+
+            if (notes.Count == 0)
                 return 0;
 
-            double maxStrain = noteDifficulties.Max();
+            double maxStrain = notes.Max();
             if (maxStrain == 0)
                 return 0;
 
-            return noteDifficulties.Sum(strain => 1.0 / (1.0 + Math.Exp(-(strain / maxStrain * 12.0 - 6.0))));
+            return notes.Sum(strain => 1.0 / (1.0 + Math.Exp(-(strain / maxStrain * 12.0 - 6.0))));
         }
 
         public double CountTopWeightedSliders(double difficultyValue)
         {
-            if (sliderStrains.Count == 0)
+            List<double> sliders = GetCurrentSliderDifficulties().Where(p => p > 0).ToList();
+
+            if (sliders.Count == 0)
                 return 0;
 
             if (noteWeightSum == 0)
@@ -126,7 +180,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
                 return 0;
 
             // Use a weighted sum of all notes. Constants are arbitrary and give nice values
-            return sliderStrains.Sum(s => DifficultyCalculationUtils.Logistic(s / consistentTopNote, 0.88, 10, 1.1));
+            return sliders.Sum(s => DifficultyCalculationUtils.Logistic(s / consistentTopNote, 0.88, 10, 1.1));
         }
     }
 }
